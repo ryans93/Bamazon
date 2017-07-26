6c51b073bbd2671e1ac3791b07e20e3923be0406
Ryan Strickler <ryanstrickler@Ryans-MacBook-Pro-2.local>
2017-07-26 13:32:24 -0400

    final revisions

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -66,13 +66,11 @@ function viewDepartmentSales() {
         head: ['department_id', 'department_name', "over_head_costs", "product_sales", "total_profit"]
     });
 
-    var groupBy = "SELECT department_name, SUM(product_sales) AS Department_Sales FROM products GROUP BY department_name;"
-    var join = "SELECT departments.`department_id`, departments.`department_name`, departments.`over_head_costs`, products.`Department-Sales` FROM departments INNER JOIN products WHERE departments.`department_name`=products.`department_name`"
+    var query = "SELECT *, SUM(product_sales) AS Department_Sales FROM products INNER JOIN departments ON products.department_name=departments.department_name GROUP BY products.department_name ORDER BY departments.department_id ASC";
 
-    connection.query(, function (err, res) {
+    connection.query(query, function (err, res) {
         if (err) return console.log(err);
-        console.log(res);
-
+        // console.log(res);
         for (var i = 0; i < res.length; i++) {
             var id = res[i].department_id;
             var name = res[i].department_name;
@@ -80,6 +78,7 @@ function viewDepartmentSales() {
             var departmentSales = res[i].Department_Sales
             var profit = parseInt(res[i].Department_Sales) - parseInt(res[i].over_head_costs)
             table.push([id, name, overHead, departmentSales, profit]);
+            if (i == res.length - 1) console.log(table.toString());
         }
     });
 }
